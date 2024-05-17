7afdfee1ce0685fc5ea45241834215301d62c7c8
DaffyFauzan <daffi.fauzan@gmail.com>
2024-05-17 17:00:35 +0700

    Added slider in index

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        playfair: ["Playfair Display", "sans-serif"],
+      },
+      letterSpacing: {
+        beranda: ["5px"],
+      }
     }
   },
   plugins: [],

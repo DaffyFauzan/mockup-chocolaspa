976b5fae1c8fd8e970900c027eb19813e0c5ed09
DaffyFauzan <daffi.fauzan@gmail.com>
2024-05-30 09:59:43 +0700

    Added Javascript function for navbar, Changed the text header, Video background in Beranda, Added Icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,60 @@ module.exports = {
       height: {
         '600':'600px',
         '400':'400px',
-      }
+      },
+      transitionDuration: {
+        '670': '670ms'
+      },
+      backgroundImage: {
+        'text-about': "url('/img/title-background.png')"
+      },
+      textShadow: {
+        'outline': '2px 2px 0 #000'
+      },
+      textStrokeWidth: {
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+      },
+      textStrokeColor: {
+        white: '#ffffff',
+        brown: '#906335',
+      },
     }
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-textshadow'),
+    function ({ addUtilities }) {
+      addUtilities({
+        '.bg-clip-text': {
+          'background-clip': 'text',
+          '-webkit-background-clip': 'text',
+        },
+        '.text-fill-transparent': {
+          'color': 'transparent',
+          '-webkit-text-fill-color': 'transparent',
+        },
+        '.text-stroke-2': {
+          '-webkit-text-stroke-width': '2px',
+        },
+        '.text-stroke-3': {
+          '-webkit-text-stroke-width': '3px',
+        },
+        '.text-stroke-4': {
+          '-webkit-text-stroke-width': '4px',
+        },
+        '.text-stroke-6': {
+          '-webkit-text-stroke-width': '6px',
+        },
+        '.text-stroke-white': {
+          '-webkit-text-stroke-color': '#ffffff',
+        },
+        '.text-stroke-brown': {
+          '-webkit-text-stroke-color': '#906335',
+        },
+      });
+    },
+  ],
 }
 
